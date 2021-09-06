ebd967f379bd8f0f2f2ed35c10e6499ab09e8126
nousvang <80865244+nousvang@users.noreply.github.com>
2021-09-05 21:58:18 -0500

    Added github url

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1> Weather App </h1>
+      <footer>
+        {" "}
+        Created by Nou Vang and is{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/nousvang/weather-react-project"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
+          opened-sourced
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
