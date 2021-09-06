262cd4c91834130a814eb0e0a9e5732daff08d23
nousvang <80865244+nousvang@users.noreply.github.com>
2021-09-05 22:53:40 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
+import React from "react";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1> Weather App </h1>
-      <footer>
-        {" "}
-        Created by Nou Vang and is{" "}
-        <a
-          href="https://github.com/nousvang/weather-react-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          opened-sourced
-        </a>
-      </footer>
+      <div lcassName="container">
+        <h1> Weather App </h1>
+        <footer>
+          {" "}
+          Created by Nou Vang and is{" "}
+          <a
+            href="https://github.com/nousvang/weather-react-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            opened-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
